e0fbaee9d3deb2875eb05829705316a9630f92b4
SejalHolam <84099041+SejalHolam@users.noreply.github.com>
2025-09-16 21:25:39 +0530

    Passing Function as props

--- OneDrive/Desktop/Web Development/React_Practice/Github/calculator-version-one/src/App.jsx
+++ OneDrive/Desktop/Web Development/React_Practice/Github/calculator-version-one/src/App.jsx
@@ -1,15 +1,28 @@
 import styles from "./App.module.css";
 import Display from "./components/Display";
 import Button from "./components/Button";
-
 import { useState } from "react";
 
 function App() {
+  const [calVal, setCalVal] = useState("");
+
+  const handleClick = (value) => {
+    if (value === "C") {
+      setCalVal("");
+    } else if (value === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newVal = calVal + value;
+      setCalVal(newVal);
+    }
+  };
+
   return (
     <>
       <div className={styles.calculator}>
-        <Display />
-        <Button />
+        <Display display={calVal} />
+        <Button onButtonClick={handleClick} />
       </div>
     </>
   );
